15f6787bc9f2a1104cc11dccdc3cfb5a772af11f
zhamalamazalieva <jamalamazalieva@gmail.com>
2020-11-16 07:59:38 +0100

    added a new hero image

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-$('.slider__item').slick({
-  dots:true,
-  arrows: false,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  responsive:[{
-    breakpoint:1024,
-    settings:{
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      dots:true
-    }}]
+$('.hero__slider').slick({
+  dots: true,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true
   });
           
